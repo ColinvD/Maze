fa830c3136e85c0cd64fb37d3ea8888f03b0422e
ColinvD <22950@ma-web.nl>
2019-09-19 13:31:42 +0200

    rooms with walls spawned

--- Maze/Assets/Scripts/GridGenerator.cs
+++ Maze/Assets/Scripts/GridGenerator.cs
@@ -7,6 +7,14 @@ public class GridGenerator : MonoBehaviour
     private int width = 5;
     private int height = 5;
     private GameObject[,] grid;
+    [SerializeField]
+    private GameObject room;
+    private GameObject[,] horizontalWalls;
+    private GameObject[,] verticalWalls;
+    [SerializeField]
+    private GameObject wall;
+    [SerializeField]
+    private Transform mazeParent;
 
     public int Width {
         get { return width; }
@@ -31,11 +39,47 @@ public class GridGenerator : MonoBehaviour
     void Start()
     {
         Grid = new GameObject[width,height];
+        horizontalWalls = new GameObject[width, height+1];
+        verticalWalls = new GameObject[width + 1, height];
+        SpawnRooms();
+        SpawnWalls();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnRooms() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Vector3 pos = new Vector3(i * 2, 0, j * 2);
+                GameObject mazeRoom = Instantiate(room, pos, new Quaternion(), mazeParent);
+                grid[i, j] = mazeRoom;
+            }
+        }
+    }
+
+    private void SpawnWalls() {
+        //horizontal
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j <= height; j++) {
+                Vector3 pos = new Vector3(i * 2, 0, -1f + j * 2);
+                GameObject mazeWall = Instantiate(wall, pos, new Quaternion(), mazeParent);
+                mazeWall.transform.localScale = new Vector3(2.5f, 1, 0.5f);
+                horizontalWalls[i,j] = mazeWall;
+            }
+        }
+
+        //vertical
+        for (int i = 0; i <= width; i++) {
+            for (int j = 0; j < height; j++) {
+                Vector3 pos = new Vector3(-1f + i * 2, 0, j * 2);
+                GameObject mazeWall = Instantiate(wall, pos, new Quaternion(), mazeParent);
+                mazeWall.transform.localScale = new Vector3(0.5f, 1, 2.5f);
+                verticalWalls[i, j] = mazeWall;
+            }
+        }
     }
 }
